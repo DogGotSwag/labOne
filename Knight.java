7f22720147202c988c9184ac9f9068bef19bd38e
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-01-30 00:01:28 -0700

    reorder translatedArrayPosition array
    better matches a real array

--- Knight.java
+++ Knight.java
@@ -1,19 +1,30 @@
 public class Knight {
     public String color;
     public int[] translatedArrayPosition = new int[2];
-
     public String letters = "abcdefgh";
     public String numbers = "87654321";
 
+    // public void generateCoordinates(){
+
+    //     int indexOne = translatedArrayPosition[0];
+    //     int indexTwo = translatedArrayPosition[1];
+
+    //     System.out.println(indexOne);
+
+    //     int[] topOne = {indexOne, indexTwo};
+    //     System.out.print(topOne[0]+", ");
+    //     System.out.print(topOne[1]);
+    //     System.out.println();
+    // }
+
     public Knight(String color, String position) {
         this.color = color;
-        translatedArrayPosition[0] = letters.indexOf(position.split("")[0]);
-        translatedArrayPosition[1] = numbers.indexOf(position.split("")[1]);
+        translatedArrayPosition[1] = letters.indexOf(position.split("")[0]);
+        translatedArrayPosition[0] = numbers.indexOf(position.split("")[1]);
     }
 
     public static void main(String[] args) {
-        Knight test = new Knight("red", "g3");
-        System.out.println(test.translatedArrayPosition[0]);
-        System.out.println(test.translatedArrayPosition[1]);
+        Knight test = new Knight("red", "g1");
+        test.generateCoordinates();
     }
 }
