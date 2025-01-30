22726be5f111199286cf717318527d40cd84a385
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-01-29 23:22:57 -0700

    transform arg positions into array positions
    letters are not useful when working with arrays

--- Knight.java
+++ Knight.java
@@ -1,9 +1,19 @@
 public class Knight {
-    public String color, position;
-    public Knight(String color, String position){
+    public String color;
+    public int[] translatedArrayPosition = new int[2];
+
+    public String letters = "abcdefgh";
+    public String numbers = "87654321";
+
+    public Knight(String color, String position) {
         this.color = color;
-        this.position = position;
+        translatedArrayPosition[0] = letters.indexOf(position.split("")[0]);
+        translatedArrayPosition[1] = numbers.indexOf(position.split("")[1]);
     }
 
-    
+    public static void main(String[] args) {
+        Knight test = new Knight("red", "g3");
+        System.out.println(test.translatedArrayPosition[0]);
+        System.out.println(test.translatedArrayPosition[1]);
+    }
 }
