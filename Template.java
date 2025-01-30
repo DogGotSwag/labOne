d8d02849c638f06e0a1fd1601f0f770b1ab48202
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-01-30 16:17:51 -0700

    make Template.java to for faster development

---
+++ Template.java
@@ -0,0 +1,62 @@
+import java.util.Vector;
+
+public class Template {  //change class name
+    public String color;
+    public String position;
+    public int[][] availableCoordinates;
+
+    public String letters = "abcdefgh";
+    public String numbers = "87654321";
+
+    public boolean inBounds(int[] coordinate) {
+        int indexOne = coordinate[0];
+        int indexTwo = coordinate[1];
+        if (indexOne < 0 || indexOne > 7)
+            return false;
+        if (indexTwo < 0 || indexTwo > 7)
+            return false;
+        return true;
+    }
+
+    public int[][] vectorToArray(Vector<int[]> vector) {
+        int[][] newArray = new int[vector.size()][2];
+        for (int i = 0; i < vector.size(); i += 1) {
+            int[] curr = { vector.get(i)[0], vector.get(i)[1] };
+            newArray[i] = curr;
+        }
+        return newArray;
+    }
+
+    public int[][] generateCoordinates() {
+        Vector<int[]> vector = new Vector<>();
+        int indexOne = numbers.indexOf(position.split("")[1]);
+        int indexTwo = letters.indexOf(position.split("")[0]);
+        // use vector.add() to add individual coordinate arrays into vector
+        // see Knight.java to see it in action
+
+        /*
+         * code here :]
+         * happy coding
+         */
+        return vectorToArray(vector);
+    }
+
+    //constructor
+    public Template(String color, String position) { //change to class name
+        this.color = color;
+        this.position = position;
+        this.availableCoordinates = generateCoordinates();
+    }
+
+
+    //main will be used test your available coordinates output
+    public static void main(String[] args) {
+        Template test = new Template("red", "g8");  //change to class name
+        System.out.println(test.color);
+        System.out.println(test.position);
+        int[][] arr = test.availableCoordinates;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i][0]+", "+arr[i][1]);
+        }
+    }
+}
