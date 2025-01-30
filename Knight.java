c472ea579d89aca464105e1f7ae9a841a1f6dcd2
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-01-29 22:50:30 -0700

    set up Knight: constructor/properties

---
+++ Knight.java
@@ -0,0 +1,9 @@
+public class Knight {
+    public String color, position;
+    public Knight(String color, String position){
+        this.color = color;
+        this.position = position;
+    }
+
+    
+}
