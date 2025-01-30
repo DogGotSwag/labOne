f67a138309992ec052865693b25e2e1708e3ff68
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-01-30 13:07:52 -0700

    set availableCoordinates property

--- Knight.java
+++ Knight.java
@@ -1,22 +1,39 @@
+import java.util.Iterator;
 import java.util.Vector;
 
 public class Knight {
     public String color;
-    public int[] translatedArrayPosition = new int[2];
+    public String position;
+    public int[][] availableCoordinates;
+
     public String letters = "abcdefgh";
     public String numbers = "87654321";
 
-    public boolean inBounds(int[] coordinate){
+    public boolean inBounds(int[] coordinate) {
         int indexOne = coordinate[0];
         int indexTwo = coordinate[1];
-        if(indexOne < 0 || indexOne > 7) return false;
-        if(indexTwo < 0 || indexTwo > 7) return false;
+        if (indexOne < 0 || indexOne > 7)
+            return false;
+        if (indexTwo < 0 || indexTwo > 7)
+            return false;
         return true;
     }
 
-    public void generateCoordinates() {
+    public int[][] vectorToArray(Vector<int[]> vector) {
+        int[][] newArray = new int[vector.size()][2];
+        for (int i = 0; i < vector.size(); i += 1) {
+            int[] curr = { vector.get(i)[0], vector.get(i)[1] };
+            newArray[i] = curr;
+        }
+        return newArray;
+    }
 
-        // Vector<int[]> vector = new Vector<>();
+    public int[][] generateCoordinates() {
+        int[] translatedArrayPosition = new int[2];
+        translatedArrayPosition[1] = letters.indexOf(position.split("")[0]);
+        translatedArrayPosition[0] = numbers.indexOf(position.split("")[1]);
+
+        Vector<int[]> vector = new Vector<>();
 
         int indexOne = translatedArrayPosition[0];
         int indexTwo = translatedArrayPosition[1];
@@ -33,18 +50,38 @@ public class Knight {
         int[] bottomLeft = { indexOne + 2, indexTwo - 1 };
         int[] bottomRight = { indexOne + 2, indexTwo + 1 };
 
-       
+        if (inBounds(topLeft) == true)
+            vector.add(topLeft);
+        if (inBounds(topRight) == true)
+            vector.add(topRight);
+        if (inBounds(leftTop) == true)
+            vector.add(leftTop);
+        if (inBounds(leftBottom) == true)
+            vector.add(leftBottom);
+        if (inBounds(rightTop) == true)
+            vector.add(rightTop);
+        if (inBounds(rightBottom) == true)
+            vector.add(rightBottom);
+        if (inBounds(bottomLeft) == true)
+            vector.add(bottomLeft);
+        if (inBounds(bottomRight) == true)
+            vector.add(bottomRight);
 
+        return vectorToArray(vector);
     }
 
     public Knight(String color, String position) {
         this.color = color;
-        translatedArrayPosition[1] = letters.indexOf(position.split("")[0]);
-        translatedArrayPosition[0] = numbers.indexOf(position.split("")[1]);
+        this.position = position;
+        this.availableCoordinates = generateCoordinates();
     }
 
     public static void main(String[] args) {
-        Knight test = new Knight("red", "g1");
-        test.generateCoordinates();
+        Knight test = new Knight("red", "g8");
+        int[][] arr = test.generateCoordinates();
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i][0] + ", " + arr[i][1]);
+        }
     }
 }
