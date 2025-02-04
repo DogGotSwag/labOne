6f393303e80a75df2ea348945a197e0bf3f4b083
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-02-03 23:40:10 -0700

    read input file and get attack position

---
+++ txttoarr.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.io.File;
+import java.util.Vector;
+
+
+//Author: Didier Longoria & Yahir Zapata
+
+public class txttoarr {
+
+    public static void main (String [] args) throws Exception {
+
+        File Pieces = new File ("./input.txt");
+        Scanner scanner = new Scanner(Pieces);
+        Vector<String> vector = new Vector<>();
+
+        while (scanner.hasNextLine()) {
+            String textLine = scanner.nextLine();
+            vector.add(textLine);
+        }
+        scanner.close();
+        Scanner getCoordinate = new Scanner(System.in);
+        System.out.print("Attack Position: ");
+        String attackPosition = getCoordinate.nextLine();
+
+
+        for (int i = 0; i < vector.size(); i++) {
+            String[] info = vector.get(i).split(", ");
+            String type = info[0];
+            String color = info[1];
+            String position = info[2]+""+info[3];
+            System.out.println(type);
+            System.out.println(color);
+            System.out.println(position);
+            System.out.println("--------");
+            
+        }
+
+    }
+
+}
