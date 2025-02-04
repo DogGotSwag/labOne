25fd1282fef20ccbbe1ab6de6322722876bc43c8
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-02-04 15:05:57 -0700

    add author comments

--- Lab1.java
+++ Lab1.java
@@ -6,8 +6,8 @@ import java.io.File;
 public class Lab1 {
 
     public static String[] txt2arr() throws Exception {
-
-        File Pieces = new File ("./input.txt");
+        // File gets read using scanner and each piece gets added as a string into an array
+        File Pieces = new File ("C:\\Users\\goofy\\OneDrive\\Documents\\Computer Science\\AOOP\\Lab1\\input.txt");
 
         Scanner scnr = new Scanner(Pieces);
 
@@ -27,16 +27,16 @@ public class Lab1 {
     }
 
     public static void main (String [] args) throws Exception {
-
+        // Scanner for user input gets created and array from file gets created using method
         Scanner userInput = new Scanner(System.in);
         String [] pieces = txt2arr();
-
+        // Columns and rows are created to get index of coordinates
         String columns = "abcdefgh";
         String rows = "87654321";
-
+        // Letter and number are used to create coordinate that user inputs
         char letter = '\0';
         int number = -1;
-
+        // Loop used to check users input (Check that input is only length 2, that the letter was inputted first, and that the letter coordinate is only from A-H)
         while (true) {
             System.out.println("Please enter the move you would like to check: (e.g., E6)");
             String userMove = userInput.nextLine();
@@ -75,26 +75,26 @@ public class Lab1 {
             }
 
         }
-        
+        // Loop used to go through all the pieces from the file
         for (int i = 0; i < pieces.length; i++) {
-            String [] attributes = pieces[i].split(", ");
+            String [] attributes = pieces[i].split(", "); // using the string that is used to save each piece we split it into each attribute (name, color, column and row)
             String name = attributes[0];
             String color = attributes[1];
             char column = attributes[2].charAt(0);
             int row = Integer.parseInt(attributes[3]);
-            String startposition = "" + column + row;
-            String nextmove = "" + number + letter;
-            boolean possible = false;
-            int[][] availableCoordinates;
+            String startposition = "" + column + row; // Using the column and row attribute we create the starting position
+            String nextmove = "" + number + letter; // Using the letter and number we created earlier we create the position for the next move
+            boolean possible = false; // This boolean is used to print out if piece was able to move or not
+            int[][] availableCoordinates; // 2d arr used to store coordinates that each piece will be able to move to
 
-            availableCoordinates = generateCoordinates(startposition, name, color);
+            availableCoordinates = generateCoordinates(startposition, name, color); // We generate each pieces available coordinates using generatecoordinates method
 
-            int indexOne = rows.indexOf(nextmove.split("")[0]);
+            int indexOne = rows.indexOf(nextmove.split("")[0]); // We get the index coordinate of the move the user inputted
             int indexTwo = columns.indexOf(nextmove.split("")[1]);
 
-            int [] move = {indexOne, indexTwo};
+            int [] move = {indexOne, indexTwo}; // We create an array of the user move
 
-            for (int j = 0; j < availableCoordinates.length; j++) {
+            for (int j = 0; j < availableCoordinates.length; j++) { // We go through the available coordinates and check if the user move is in there, boolean (possible) will be changed to true if found
                 if (Arrays.equals(move, availableCoordinates[j])) {
                     possible = true;
                     break;
@@ -103,7 +103,7 @@ public class Lab1 {
 
             }
                 
-            if (possible) {
+            if (possible) { // If boolean (possible) is true we print that the move is possible
                 System.out.println(name + " at " + column + ", " + row + " can move to " + letter + ", " + number);
 
             }
@@ -116,7 +116,7 @@ public class Lab1 {
         }
 
     }
-
+    // Method used to check if coordinates in generatecoordinates are inbound of chess board
     public static boolean inBounds(int[] coordinate) {
         int indexOne = coordinate[0];
         int indexTwo = coordinate[1];
@@ -126,7 +126,7 @@ public class Lab1 {
             return false;
         return true;
     }
-
+    // Method used to convert vector into an array
     public static int[][] vectorToArray(Vector<int[]> vector) {
         int[][] newArray = new int[vector.size()][2];
         for (int i = 0; i < vector.size(); i += 1) {
@@ -135,13 +135,14 @@ public class Lab1 {
         }
         return newArray;
     }
-
+    // Main method that is used to get available coordinates (or moves) that each piece can go to, uses the starting position, name and color of each piece
     public static int[][] generateCoordinates(String position, String name, String color) {
-        Vector<int[]> vector = new Vector<>();
+        Vector<int[]> vector = new Vector<>(); // Variables used for all pieces which are a vector, and the two strings letters and numbers which are used to get the coordinates of the starting position
         String letters = "abcdefgh";
         String numbers = "87654321";
-
-        if (name.equals("King")) {
+        // We use if cases to check which piece we are dealing with depending on the name that is inputted to the method
+        if (name.equals("King")) { 
+            //author: Didier Longoria
 
             int rows = numbers.indexOf(position.split("")[1]);
             int cols = letters.indexOf(position.split("")[0]);
@@ -216,6 +217,8 @@ public class Lab1 {
         }
 
         else if (name.equals("Knight")) {
+            //author : Didier Longoria
+
             int rows = numbers.indexOf(position.split("")[1]);
             int cols = letters.indexOf(position.split("")[0]);
 
@@ -251,6 +254,7 @@ public class Lab1 {
         }
 
         else if (name.equals("Queen")) {
+            //author: Didier Longoria
             int rows = numbers.indexOf(position.split("")[1]);
             int cols = letters.indexOf(position.split("")[0]);
 
@@ -313,18 +317,18 @@ public class Lab1 {
 
         else if (name.equals("Bishop")) {
 
-            int indexOne = numbers.indexOf(position.split("")[1]);
+            int indexOne = numbers.indexOf(position.split("")[1]); // We get the coordinate of the starting position
             int indexTwo = letters.indexOf(position.split("")[0]);
         
             //Top Left
-            int x = indexOne;
+            int x = indexOne; // We copy the index of the starting position using x and y variable
             int y = indexTwo;
 
-            while (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
-                x = x + 1;
+            while (x >= 0 && x <= 7 && y >= 0 && y <= 7) { // Using x and y we traverse the board using if statements
+                x = x + 1; // Since we are only adding coordinates to the top left of the bishop we go up 1 and left 1
                 y = y - 1;
 
-                int [] topLeft = {x, y};
+                int [] topLeft = {x, y}; // We create an array of the coordinate and add it to the vector if it's inbounds
 
                 if (inBounds(topLeft) == true)
                 vector.add(topLeft);
@@ -332,14 +336,14 @@ public class Lab1 {
             }
 
             //Top Right
-            x = indexOne;
+            x = indexOne; // Index gets reset
             y = indexTwo;
 
-            while (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
-                x = x + 1;
+            while (x >= 0 && x <= 7 && y >= 0 && y <= 7) { // Using x and y we traverse the board using if statements
+                x = x + 1; // Now that we are going to the top right we go up by 1 and right 1
                 y = y + 1;
 
-                int [] topRight = {x, y};
+                int [] topRight = {x, y}; // We create an array of the coordinate and add it to the vector if it's inbounds
 
                 if (inBounds(topRight) == true)
                 vector.add(topRight);
@@ -347,14 +351,14 @@ public class Lab1 {
             }
 
             //Bottom Left
-            x = indexOne;
+            x = indexOne; // Index gets reset
             y = indexTwo;
 
-            while (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
-                x = x - 1;
+            while (x >= 0 && x <= 7 && y >= 0 && y <= 7) { // Using x and y we traverse the board using if statements
+                x = x - 1; // For bottom left we go down 1 and to the left 1
                 y = y - 1;
 
-                int [] bottomLeft = {x, y};
+                int [] bottomLeft = {x, y}; // We create an array of the coordinate and add it to the vector if it's inbounds
 
                 if (inBounds(bottomLeft) == true)
                 vector.add(bottomLeft);
@@ -362,14 +366,14 @@ public class Lab1 {
             }
 
             //Bottom Right
-            x = indexOne;
+            x = indexOne; // Index gets reset
             y = indexTwo;
 
-            while (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
-                x = x - 1;
+            while (x >= 0 && x <= 7 && y >= 0 && y <= 7) { // Using x and y we traverse the board using if statements
+                x = x - 1; // For bottom right we go down 1 and to the right 1
                 y = y + 1;
 
-                int [] bottomRight = {x, y};
+                int [] bottomRight = {x, y}; // We create an array of the coordinate and add it to the vector if it's inbounds
 
                 if (inBounds(bottomRight) == true)
                 vector.add(bottomRight);
@@ -377,7 +381,7 @@ public class Lab1 {
             }
 
         }
-
+        // We return vector using vectortoarray method which will convert it to array
         return vectorToArray(vector);
 
     }
